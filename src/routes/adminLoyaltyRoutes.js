56fba01365450fe24471a78ca527d0e5a17c2cac
vinaanurfauziah <vinaanurfauziah@gmail.com>
2026-07-06 13:34:10 +0700

    feat: add marketing dashboard with redeem access

--- src/routes/adminLoyaltyRoutes.js
+++ src/routes/adminLoyaltyRoutes.js
@@ -29,42 +29,43 @@ const {
 } = require('../controllers/adminLoyaltyController');
 
 const router = express.Router();
-const adminOnly = [auth, requireRole('admin', 'staff')];
+const adminStaffOnly = requireRole('admin', 'staff');
 const superAdminOnly = requireRole('admin');
+const marketingOnly = requireRole('admin', 'staff', 'marketing');
 
-router.use(...adminOnly);
+router.use(auth);
 
 router.get('/users', superAdminOnly, listAdminUsers);
 router.post('/users', superAdminOnly, createAdminUser);
 router.put('/users/:id', superAdminOnly, updateAdminUser);
 router.delete('/users/:id', superAdminOnly, deleteAdminUser);
 
-router.get('/customers', listAdminCustomers);
-router.post('/customers', createAdminCustomer);
-router.put('/customers/:id', updateAdminCustomer);
-router.delete('/customers/:id', deleteAdminCustomer);
+router.get('/customers', adminStaffOnly, listAdminCustomers);
+router.post('/customers', adminStaffOnly, createAdminCustomer);
+router.put('/customers/:id', adminStaffOnly, updateAdminCustomer);
+router.delete('/customers/:id', adminStaffOnly, deleteAdminCustomer);
 
-router.get('/brands', listAdminBrands);
-router.get('/locations', listAdminLocations);
+router.get('/brands', adminStaffOnly, listAdminBrands);
+router.get('/locations', adminStaffOnly, listAdminLocations);
 
-router.get('/loyalty-summary', getSummary);
+router.get('/loyalty-summary', marketingOnly, getSummary);
 
-router.get('/rewards', listRewards);
-router.post('/rewards', createReward);
-router.put('/rewards/:id', updateReward);
+router.get('/rewards', adminStaffOnly, listRewards);
+router.post('/rewards', adminStaffOnly, createReward);
+router.put('/rewards/:id', adminStaffOnly, updateReward);
 
-router.get('/catalog/menu-items', listCatalogMenuItems);
+router.get('/catalog/menu-items', marketingOnly, listCatalogMenuItems);
 
-router.get('/redeem-menu/categories', listRedeemCategories);
-router.post('/redeem-menu/categories', createRedeemCategory);
-router.put('/redeem-menu/categories/:id', updateRedeemCategory);
+router.get('/redeem-menu/categories', marketingOnly, listRedeemCategories);
+router.post('/redeem-menu/categories', marketingOnly, createRedeemCategory);
+router.put('/redeem-menu/categories/:id', marketingOnly, updateRedeemCategory);
 
-router.get('/redeem-menu/items', listRedeemItems);
-router.post('/redeem-menu/items', createRedeemItem);
-router.put('/redeem-menu/items/:id', updateRedeemItem);
-router.delete('/redeem-menu/items/:id', deleteRedeemItem);
+router.get('/redeem-menu/items', marketingOnly, listRedeemItems);
+router.post('/redeem-menu/items', marketingOnly, createRedeemItem);
+router.put('/redeem-menu/items/:id', marketingOnly, updateRedeemItem);
+router.delete('/redeem-menu/items/:id', marketingOnly, deleteRedeemItem);
 
-router.get('/redemptions', listRedemptions);
-router.put('/redemptions/:id/status', updateRedemptionStatus);
+router.get('/redemptions', adminStaffOnly, listRedemptions);
+router.put('/redemptions/:id/status', adminStaffOnly, updateRedemptionStatus);
 
 module.exports = router;
