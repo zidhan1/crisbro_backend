682ba6d67479faa150a3e109e58c28338cf2397e
user <user@MacBook-Pro.local>
2026-08-27 22:40:33 +0700

    create payload webhook console.log

--- src/routes/qontak.routes.js
+++ src/routes/qontak.routes.js
@@ -3,12 +3,15 @@ const router = express.Router();
 
 router.post("/733f8d9cc06104f3", async (req, res) => {
   const { verify_info } = req.body;
+  const payload = req.body;
 
   if (!verify_info) {
     return res.status(400).json({
       success: false,
     });
   }
+
+  console.log(payload);
 
   return res.status(200).json({
     success: true,
